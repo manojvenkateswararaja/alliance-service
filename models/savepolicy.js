8efd326102c05a3b1a7588b66b775d57c18f79c5
arun1793 <arun.hossamani@rapidqube.com>
2017-09-27 10:38:53 +0530

    Update savepolicy.js

--- models/savepolicy.js
+++ models/savepolicy.js
@@ -8,23 +8,17 @@ const policySchema = mongoose.Schema({
     id: String,
     consignmentWeight: Number,
     consignmentValue: Number,
-    
+
     modeofTransport: String,
     packingMode: String,
     consignmentType: String,
     contractType: String,
     policyType: String,
-   
-    invoiceNo: Number,
-   
 
-
+    invoiceNo: Number
 });
-
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://rpqbci:rpqb123@ds163721.mlab.com:63721/commercialinsurance', {
-    useMongoClient: true
-});
+mongoose.connect('mongodb://rpqbci:rpqb123@ds163721.mlab.com:63721/commercialinsurance', {useMongoClient: true});
 
 module.exports = mongoose.model('savepolicy', policySchema);
